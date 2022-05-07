33a88d2e771481f524fd910695e1adffdf1b811f
Tyler Hervey <hervey711@gmail.com>
2022-05-06 19:31:14 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import './categories.styles.scss'
+
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      subtitle: "Shop Now",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      subtitle: "Shop Now",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      subtitle: "Shop Now",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      subtitle: "Shop Now",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      subtitle: "Shop Now",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title, subtitle }) => (
+        <div className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>{subtitle}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
